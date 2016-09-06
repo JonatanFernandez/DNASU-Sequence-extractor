04f58dca7a0155dcc1e3fad34d23ec36f24f4352
Ouuiea <jonifg@usal.es>
2016-09-06 13:42:02 +0200

    Update DNASU-1.0-EngCode.py

--- DNASU-1.0-EngCode.py
+++ DNASU-1.0-EngCode.py
@@ -4,12 +4,12 @@ Version 1.0 - 5 of september 2016.
 This script extracts plasmids sequences from DNASU. Takes a CSV file with a plasmid ID
 in each row, and returns a new file with each plasmid sequence in the same position.
 
-The script is far from error-proof. It considers blank spaces in the csv, but other than that,
+The script is far from error-proof. It considers blank spaces in the csv, but none other than that:
 it doesn't consider the possibility of various results in the DNASU Search, for example.
 
-By default the input file name is "DNASU_IDS.csv" (Place it in the Python working directory before
-exceuting the script). 
-In line 77 you can modify the input file name. The output file name is DNASU_out.csv, again,you can 
+By default the input file name is "DNASU_IDS.csv" (Place the file in the Python working directory before
+executing the script). 
+In line 77 you can modify the input file name. The output file name is DNASU_out.csv, again, you can 
 modify it in line 79.
 
 """
